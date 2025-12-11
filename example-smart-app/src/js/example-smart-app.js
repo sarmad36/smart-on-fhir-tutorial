10a8c4b8d4ddb3a7030012bcec87263854d7b080
Sarmad Nazir <33855923+sarmad36@users.noreply.github.com>
2025-12-11 17:00:03 +0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -33,7 +33,8 @@
 
           if (typeof patient.name[0] !== 'undefined') {
             fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family.join(' ');
+            // lname = patient.name[0].family.join(' ');
+            lname = '';
           }
 
           var height = byCodes('8302-2');
